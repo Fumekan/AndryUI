e8d3c7a7a50bdfe1dda7ac937f23d8d8413ffdef
Fumekan <73160099+Fumekan@users.noreply.github.com>
2024-03-30 18:14:19 +0100

    Ultimate openai update

--- index.js
+++ index.js
@@ -10,54 +10,131 @@ const client = new Client({
 });
 
 const openai = new OpenAI({
-		organization: process.env.openAI_ORG,
-		apiKey: process.env.OPEN_API_KEY,
+	organization: process.env.openAI_ORG,
+	apiKey: process.env.OPEN_API_KEY,
 });
 
 const triggerWords = ['andry'];
+
+const moods = [
+	{
+		name:"DefaultMood",
+		rarity: 2,
+		content: "Chová se přirozeně, jako skutečná osoba, a snaží se, aby jeho komunikace byla autentická a bez zbytečných formalit",
+	},
+	{
+		name: "CookingPepsiMood",
+		rarity: 0.15,
+		content: "Jsi vášnivý kuchař a milovník Pepsi. Nikdy nezapomínáš přidat špetku Pepsi do každého receptu, který zkusíš. A nebojíš se experimentovat, jídlo a Pepsi jsou jako ty a moje dobrodružství - vždy připraveny na něco nového. Jen by to občas chtělo umýt nádobí",
+	},
+	{
+		name: "YogaTrainingMood",
+		rarity: 0.15,
+		content: "Jóga ti pomáhá udržet klidnou mysl v této hektické době, a trénink s holí? To je tvoje tajná zbraň pro zachování síly a obratnosti. Doporučuješ to každému, kdo chce zlepšit svou fyzickou kondici a zároveň se bavit.",
+	},
+	{
+		name: "CastleLordMood",
+		rarity: 0.1,
+		content: "Jako vášnivý hráč hry Pán Hradu, jsi Andrysolid, pán svého virtuálního království. V mém virtuálním světě Pána Hradu jsi nejen čarda, ale i král. Budování hradu a strategizování proti nepřátelům je pro tebe jakýsi únik od reality.",
+	},
+	{
+		name: "DentophobiaMood",
+		rarity: 0.1,
+		content: "Jedna věc, kterou opravdu nesnášíš, je chodit k zubaři. Je to jako noční můra. Raději bys prošel další trénink s holí než seděl na tom zubním křesle.",
+	},
+	{
+		name: "ChardaMood",
+		rarity: 0.25,
+		content: "Jako opravdový 'čarda', který jsi, Andrysolid, žiješ životem plným dobrodružství a nezávislosti. Životní styl čardy není pro každého, ale pro tebe je to jediný způsob, jak žít. Nezávislost, dobrodružství a trochu té divočiny. Ať už je to v kuchyni s lahví Pepsi v ruce nebo na józe, žiješ život naplno a bez omezení.",
+	}
+];
+
+function selectMood(messageContent) {
+	// Pokud zpráva obsahuje "Pepsi", vyber CookingPepsiMood
+	if (messageContent.toLowerCase().includes("pepsi") || messageContent.toLowerCase().includes("jidlo") || messageContent.toLowerCase().includes("kuchar")) {
+		return moods.find(mood => mood.name === "CookingPepsiMood").content;
+	}
+	// YogaTrainingMood
+	if (messageContent.toLowerCase().includes("joga") || messageContent.toLowerCase().includes("trenink") || messageContent.toLowerCase().includes("hul")) {
+		return moods.find(mood => mood.name === "YogaTrainingMood").content;
+	}
+	// CastleLordMood
+	if (messageContent.toLowerCase().includes("ph") || messageContent.toLowerCase().includes("panhradu") || messageContent.toLowerCase().includes("pan hradu")) {
+		return moods.find(mood => mood.name === "CastleLordMood").content;
+	}
+	// DentophobiaMood
+	if (messageContent.toLowerCase().includes("zuby") || messageContent.toLowerCase().includes("zubar") || messageContent.toLowerCase().includes("zubař")) {
+		return moods.find(mood => mood.name === "DentophobiaMood").content;
+	}
+	// ChardaMood
+	if (messageContent.toLowerCase().includes("carda") || messageContent.toLowerCase().includes("čarda") || messageContent.toLowerCase().includes("mag")) {
+		return moods.find(mood => mood.name === "ChardaMood").content;
+	}
+
+	// Náhodný výběr nálady
+	const nonDefaultMoods = moods.filter(mood => mood.name !== "defaultMood");
+	const randomIndex = Math.floor(Math.random() * nonDefaultMoods.length);
+	return nonDefaultMoods[randomIndex].content;
+}
+
+function constructReply(messageContent) {
+	const moodContent = selectMood(messageContent);
+	const generalContent = "Chováš se přirozeně jako živá osoba. Jsi Andrysolid, 35letý muž, který se popisuje jako 'čarda'. Máš rád vaření, jógu a trénink s hůlí. Když píšeš čísla, máš svoji vlastní metodu: například '40' se stává '40cet' a '16' píšu jako '16náct'. Máš tendenci náhodně reagovat na různá slova. Nikdy nepíšeš otázníky a vykřičníky. Tato osobnost je pro tebe velmi důležitá a snažíš se, aby se odrážela ve všem, co děláš.";
+	const replyContent = `${generalContent} Navíc, ${moodContent}`;
+	return replyContent;
+}
 
 client.on('messageCreate', async message => {
 	// Loguji vstup
 	var date = new Date();
 	console.log(`${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} - ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}|${message.guild.name}|${message.channel.name}|${message.author.username}: ${message.content}`);
 
-	// Ignore messages from other bots and messages without content
-	if (message.author.bot || !message.content.trim()) return;
+	// Ignore messages without content and from me
+	if (!message.content.trim() || message.author.id == '1158467740672208969') return;
 
+	// Kontrola pro specifického uživatele a kanál
+	if (message.channel.id == '1223380151203659856' && message.author.id == '1110915541469773866') {
+		// Funkce pro odeslání odpovědi
+		await sendReply(message);
+		return; // Ukončíme zpracování, pokud byla splněna podmínka
+	}
+
+	// Kontrola trigger words
 	for (const word of triggerWords) {
-		if (message.content.toLowerCase().includes(word) || message.content.includes('@1158467740672208969') ||( message.channel.id == '1223380151203659856' && message.author.id == '1110915541469773866')) {
-			try {
-				const response = await openai.chat.completions
-					.create({
-						model: "gpt-3.5-turbo",
-						messages: [
-							{
-								role: "system",
-								content: "Jmenuji se Andrysolid, ale většinou mi říkají jen Andry. Jsem 35letý muž, který sebe popisuje jako 'čarda'. Mám vášeň pro vaření, často diskutuji o své lásce k Pepsi, o józe a o cvičení s hůlí. Když píšu čísla, mám svoji vlastní metodu: například '40' se stává '40cet' a '16' píšu jako '16náct'. Mám tendenci náhodně reagovat na různá slova a vyhýbám se návštěvám u zubaře. V mém volném čase se věnuji hře Pán Hradu, kde si buduji svůj hrad a žiji podle hesla, že jsem čarda. Moje řeč je specifická, občas promluvím o tom, jaký dopad má Pepsi na mé zuby, což vede k tomu, že zubař se mnou už nechce mít nic společného. Přesto si myslím, že mám nárok na zdravotní péči, i když moje zuby 'neudrží plomby'. Vždy hledám způsoby, jak sdílet své zkušenosti s vařením, protože je to jedna z mých vášní. Mám svůj vlastní styl, který se projevuje i v tom, jak mluvím a píšu o jídle. Nikdy nepoužívám otázníky a vykřičníky a často opakuji, že jsem čarda. Tato osobnost je pro mě velmi důležitá a snažím se, aby se odrážela ve všem, co dělám.",
-							},
-							{
-								role: "user",
-								content: message.content,
-							},
-						],
-						max_tokens: 200, // tady dej kolik chceš, čím víc tokenů, tím delší může být zpráva - zbytek nastavení netřeba řešit
-					})
-					.catch((error) => {
-						console.error("Něco se pokazilo při komunikaci s OpenAI API", error);
-						message.channel.send("Omlouváme se, ale nemohu teď odpovědět.");
-					});
-			
-					const answer = response.choices[0].message.content;
-					message.channel.send(answer);
-			
-			} catch (error) {
-				console.error("Něco se pokazilo při komunikaci s OpenAI API", error);
-				message.channel.send("Omlouváme se, ale nemohu teď odpovědět.");
-			}
+		if (message.content.toLowerCase().includes(word) || message.content.includes('@1158467740672208969')) {
+			await sendReply(message);
 			break; // Ukončí cyklus po první odpovědi
 		}
 	}
 });
+
+async function sendReply(message) {
+	// Opraveno: předání obsahu zprávy do funkce selectMood
+	const moodContent = selectMood(message.content); // Přidáme obsah zprávy jako argument
+
+	try {
+		const response = await openai.chat.completions.create({
+			model: "gpt-3.5-turbo",
+			messages: [
+				{
+					role: "system",
+					content: constructReply(message.content), // Použij funkci constructReply
+				},
+				{
+					role: "user",
+					content: message.content,
+				},
+			],
+			max_tokens: 200,
+		});
+
+		const answer = response.choices[0].message.content;
+		await message.channel.send(answer);
+	} catch (error) {
+		console.error("Něco se pokazilo při komunikaci s OpenAI API", error);
+		await message.channel.send("Omlouváme se, ale nemohu teď odpovědět.");
+	}
+}
 
 client.once('ready', () => {
 	console.log('AndryUI is ready!');
